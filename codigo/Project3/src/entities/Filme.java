2a00bdc1b729b86314bd428016d7698c275f7291
kaiohs333 <kaiohsilveira@gmail.com>
2023-05-01 14:06:29 -0300

    Implementacao inicial, classe filme

--- codigo/Project3/src/entities/Filme.java
+++ codigo/Project3/src/entities/Filme.java
@@ -2,4 +2,49 @@ package entities;
 
 public class Filme {
     
+    private int id;
+    private String nome;
+    private LocalDate dataLancamento;
+    private int duracaoMinutos;
+    
+    public Filme(int id, String nome, LocalDate dataLancamento, int duracaoMinutos) {
+        this.id = id;
+        this.nome = nome;
+        this.dataLancamento = dataLancamento;
+        this.duracaoMinutos = duracaoMinutos;
+    }
+    
+    // getters e setters
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+    
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+    
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+    
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
 }
