53924743279601be8f8205a8ed077d963adec05e
Vítor Lion <102547363+Lvion@users.noreply.github.com>
2023-05-01 22:52:17 -0300

    Commit da classe app com menu inicial

--- codigo/Project3/src/app/App.java
+++ codigo/Project3/src/app/App.java
@@ -19,6 +19,8 @@ import entities.Serie;
 public class App {
 
 	public static void main(String[] args) throws ParseException {
+		
+		int opcao;
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -58,7 +60,37 @@ public class App {
 		
 		System.out.println(DataLoader.listClientes.get(0).getListaJaVistas());		
 		
-		
-		
+		//Menu para chamar os métodos
+		   Scanner scanner = new Scanner(System.in);
+
+	        do {
+	            System.out.println("Escolha uma opção:");
+	            System.out.println("1 - ");
+	            System.out.println("2 - ");
+	            System.out.println("3 - ");
+	            System.out.println("0 - Sair");
+
+	            opcao = scanner.nextInt();
+
+	            switch(opcao) {
+	                case 1:
+	                    
+	                    break;
+	                case 2:
+	                    
+	                    break;
+	                case 3:
+	                    
+	                    break;
+	                case 0:
+	                    System.out.println("Saindo...");
+	                    break;
+	                default:
+	                    System.out.println("Opção inválida!");
+	            }
+	        } while (opcao != 0);
+
+	        scanner.close();
 	}
 }
+
