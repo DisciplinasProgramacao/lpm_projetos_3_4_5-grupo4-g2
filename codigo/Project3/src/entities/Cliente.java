d18587d671065cba0cb86c09fe8840e0271d5668
kaiohs333 <kaiohsilveira@gmail.com>
2023-05-17 20:22:48 -0300

    Update - Avaliaçao por comments e tipos de cliente

--- codigo/Project3/src/entities/Cliente.java
+++ codigo/Project3/src/entities/Cliente.java
@@ -1,12 +1,15 @@
 package entities;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 class Cliente {
     private String nomeDoUsuario;
     private String senha;
+    private boolean especialista;
     private List<Midia> listaParaVer;
     private List<Midia> listaAssistidas;
+    private List<Avaliacao> listaAvaliadas = new ArrayList<>();
 
     public Cliente(String nomeDoUsuario, String senha) {
         this.nomeDoUsuario = nomeDoUsuario;
@@ -14,6 +17,36 @@ class Cliente {
         this.listaParaVer = new ArrayList<>();
         this.listaAssistidas = new ArrayList<>();
     }
+
+    //Atualiza o status do cliente para tipo Especialista
+
+    /**
+     * É utilizado a classe padrão LocalDate para obter a data atual e a data do mês anterior. 
+     * Em seguida, contamos a quantidade de mídias assistidas pelo cliente no mês anterior e atualizamos seu status 
+     * de especialista se necessário.
+     */
+    public void atualizarStatusEspecialista() {
+        // Obtém a data atual
+        LocalDate dataAtual = LocalDate.now();
+        // Define a data de referência como sendo o mês anterior
+        LocalDate dataReferencia = dataAtual.minusMonths(1);
+        
+        // Conta quantas mídias o cliente assistiu no mês anterior
+        int quantidadeAssistidas = 0;
+        for (Midia midia : listaAssistidas) {
+            LocalDate dataAssistida = midia.getDataAssistida();
+            if (dataAssistida != null && dataAssistida.isAfter(dataReferencia) && dataAssistida.isBefore(dataAtual)) {
+                quantidadeAssistidas++;
+            }
+        }
+        
+        // Atualiza o status de especialista se necessário
+        if (quantidadeAssistidas >= 5) {
+            especialista = true;
+            System.out.println("Parabéns! Você atingiu o status de especialista.");
+        }
+    }
+    
 
     public void adicionarNaListaParaVer(Midia midia) {
         listaParaVer.add(midia);
@@ -72,6 +105,44 @@ class Cliente {
             midia.adicionarAvaliacao(novaAvaliacao);
         } else {
             System.out.println("Avaliação inválida. A avaliação deve ser um número entre 1 e 5.");
+        }
+    }
+
+
+    //Lista de avaliações
+
+    public void adicionarAvaliacao(Avaliacao avaliacao) {
+        listaAvaliadas.add(avaliacao);
+    }
+    
+    public List<Avaliacao> getListaAvaliadas() {
+        return listaAvaliadas;
+    }
+    
+    public double calcularMediaAvaliacoes() {
+        double soma = 0.0;
+        for (Avaliacao avaliacao : listaAvaliadas) {
+            soma += avaliacao.getNota();
+        }
+        if (listaAvaliadas.size() == 0) {
+            return 0.0;
+        }
+        return soma / listaAvaliadas.size();
+    }
+    
+
+    //Metodo para avaliar atraves de um comentario 
+
+    public void avaliarComentario(Midia midia, int avaliacao, String comentario) {
+        if (especialista) {
+            avaliar(midia, avaliacao);
+            if (listaAvaliadas.contains(midia)) {
+                Avaliacao avaliacaoAtual = midia.getAvaliacao();
+                avaliacaoAtual.setComentario(comentario);
+                System.out.println("Avaliação com comentário registrada com sucesso!");
+            }
+        } else {
+            System.out.println("Você não possui permissão para adicionar comentários.");
         }
     }
 
